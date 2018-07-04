27cf3b5a4dc47705e611891570a699cab65ac7fa
Eric Shuman <IntuitiveLeap@users.noreply.github.com>
2018-07-04 02:16:54 -0400

    -got .lvl1 to .lvl2 operating correctly.

--- js/app.js
+++ js/app.js
@@ -22,40 +22,51 @@ $(function() {
     });
 
     $('.lvl1').on('click', function() {
-        var $self = $(this);
-        if ($self.children().find('.lvl2').hasClass('hide')) {
-            $self.find('.lvl2').removeClass('hide');
-            $self.siblings().find('.lvl2').addClass('hide');
-            return false;
+        var $thisItem = $(this);
+        if ($thisItem.find('.lvl2').length === 0) {
+            console.log($thisItem + 'has no nested items.')
+            return false;                                    // no nested <li>? do nothing.
+        }else if(!$thisItem.find('.lvl2').hasClass('hide')) {
+            console.log($thisItem + "is showing it's sub-items. Hiding them now.")
+            $thisItem.find('li').addClass('hide');           // nested <li> is showing? hide it and its children <li> (just in case they are peeking).
         }else{
-            $self.find('.lvl2').addClass('hide');
-            $self.siblings().find('.lvl2').addClass('hide');
+            console.log($thisItem + "is hiding it's sub-items. Showing them now.")
+            $thisItem.find('.lvl2').removeClass('hide');
+            $thisItem.siblings().find('li').addClass('hide');
+            return false;                                    // nested <li> is hidden? show nested <li> + hide sibling <li>.
         }
     });
 
     $('.lvl2').on('click', function() {
         var $self = $(this);
+        if ($self.find('.lvl3').length === 0) {
+            return false;
+        }
+        if (!$self.find('.lvl3').hasClass('hide')) {
+            $self.find('.lvl3').addClass('hide');
+        }
         if ($self.children().find('.lvl3').hasClass('hide')) {
             $self.find('.lvl3').removeClass('hide');
-            $self.siblings().find('.lvl3').addClass('hide');
+            $self.siblings().find('li').addClass('hide');
             return false;
         }else{
             $self.find('.lvl3').addClass('hide');
-            $self.siblings().find('.lvl3').addClass('hide');
+            $self.siblings().find('li').addClass('hide');
         }
     });
 
     $('.lvl3').on('click', function() {
         var $self = $(this);
-        if ($self.find('.lvl4').length === 0) {
+        if ($self.find('.lvl4').length === 0 | !$self.find('.lvl4').hasClass('hide')) {
             return false;
-        }else if ($self.children().find('.lvl4').hasClass('hide')) {
+        }
+        if ($self.children().find('.lvl4').hasClass('hide')) {
             $self.find('.lvl4').removeClass('hide');
-            $self.siblings().find('.lvl4').addClass('hide');
+            $self.siblings().find('li').addClass('hide');
             return false;
         }else{
             $self.find('.lvl4').addClass('hide');
-            $self.siblings().find('.lvl4').addClass('hide');
+            $self.siblings().find('li').addClass('hide');
         }
     });
 // end of script.
