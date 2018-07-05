4891577d23c621a41a2f62850152b183f97616d3
Eric Shuman <IntuitiveLeap@users.noreply.github.com>
2018-07-04 23:13:17 -0400

    linted app.js

--- js/app.js
+++ js/app.js
@@ -1,76 +1,82 @@
-$(function() {
-
-    var $inner = $('.myBrand .inner');
-    var $app = $('.myBrand .app');
-    var $blurb = $('.myBrand .blurb');
-    var $btn1 = $('.myBrand .myButton1');
+/*jslint
+  this
+*/
+/*global
+  $
+*/
+$(function () {
+    "use strict";
+    //var $inner = $(".myBrand .inner");
+    //var $app = $(".myBrand .app");
+    //var $btn1 = $(".myBrand .myButton1");
+    var $blurb = $(".myBrand .blurb");
 
     if ($blurb.is(":hidden")) {
         $blurb.slideDown(600);
     }
 
-    var $showInfoBtn = $('#showInfoBtn');
-    var $extrasPanel = $('.extrasPanel');
-    $showInfoBtn.on('click', function() {
+    var $showInfoBtn = $("#showInfoBtn");
+    var $extrasPanel = $(".extrasPanel");
+    $showInfoBtn.on("click", function () {
         if ($extrasPanel.is(":hidden")) {
             $extrasPanel.show(400);
             $showInfoBtn.html('Hide "Extra Info" &Uarr; ');
-        }else{
+        } else {
             $extrasPanel.hide(400);
             $showInfoBtn.html('Show "Extra Info" &Darr; ');
         }
     });
 
-    $('.lvl1').on('click', function() {
+    $('.lvl1').on('click', function () {
         var $thisItem = $(this);
         if ($thisItem.find('.lvl2').length === 0) {
             return false;                                    // no nested <li>? do nothing.
-        }else if(!$thisItem.find('.lvl2').hasClass('hide')) {
+        } else if (!$thisItem.find('.lvl2').hasClass('hide')) {
             $thisItem.children().find('li').addClass('hide');           // nested <li> is showing? hide thisItem's descendant <li>.
             return false;
-        }else{
+        } else {
             $thisItem.find('.lvl2').removeClass('hide');
             $thisItem.siblings().find('li').addClass('hide');
             return false;                                    // nested <li> is hidden? show thisItem's nested <li> + hide siblings' nested <li>.
         }
     });
 
-    $('.lvl2').on('click', function() {
+    $('.lvl2').on('click', function () {
         var $thisItem = $(this);
         if ($thisItem.find('.lvl3').length === 0) {
             return false;                                    // no nested <li>? do nothing.
-        }else if(!$thisItem.find('.lvl3').hasClass('hide')) {
+        } else if (!$thisItem.find('.lvl3').hasClass('hide')) {
             $thisItem.children().find('li').addClass('hide');           // nested <li> is showing? hide thisItem's descendant <li>.
             return false;
-        }else{
+        } else {
             $thisItem.find('.lvl3').removeClass('hide');
             $thisItem.siblings().find('li').addClass('hide');
             return false;                                    // nested <li> is hidden? show thisItem's nested <li> + hide thisItem's sibling <li>.
         }
     });
 
-    $('.lvl3').on('click', function() {
+    $('.lvl3').on('click', function () {
         var $thisItem = $(this);
         if ($thisItem.find('.lvl4').length === 0) {
             return false;                                    // no nested <li>? do nothing.
-        }else if(!$thisItem.find('.lvl4').hasClass('hide')) {
+        } else if (!$thisItem.find('.lvl4').hasClass('hide')) {
             $thisItem.children().find('li').addClass('hide');           // nested <li> is showing? hide its children <li> (just in case they are peeking).
             return false;
-        }else{
+        } else {
             $thisItem.find('.lvl4').removeClass('hide');
             $thisItem.siblings().find('li').addClass('hide');
             return false;                                    // nested <li> is hidden? show nested <li> + hide sibling <li>.
         }
     });
 
-    $('.lvl4').on('click', function() {
+    $('.lvl4').on('click', function () {
         var $thisItem = $(this);
         if ($thisItem.find('.lvl5').length === 0) {
             return false;                                    // no nested <li>? do nothing.
-        }else if(!$thisItem.find('.lvl5').hasClass('hide')) {
+        } else if (!$thisItem.find('.lvl5').hasClass('hide')) {
             $thisItem.children().find('li').addClass('hide');           // nested <li> is showing? hide its children <li> (just in case they are peeking).
             return false;
-        }else{
+        } else {
             $thisItem.find('.lvl5').removeClass('hide');
             $thisItem.siblings().find('li').addClass('hide');
             return false;                                    // nested <li> is hidden? show nested <li> + hide sibling <li>.
